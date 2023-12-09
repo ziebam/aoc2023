3a156eb26c31ec4293d2ddf4127cdaaea12ee5dc
ziebam <ziebamichal@tutanota.com>
2023-12-09 13:08:31 +0100

    Add naive solution for day 9

---
+++ day9/day9.py
@@ -0,0 +1,54 @@
+from performance_utils.performance_utils import measure_performance
+
+with open("day9/in9.txt") as in9:
+    data = [line.strip() for line in in9.readlines()]
+
+
+def part1(data):
+    out = 0
+    histories = [history.split() for history in data]
+    for history in histories:
+        history = [int(v) for v in history]
+        sequences = [history]
+        while not all(v == 0 for v in sequences[-1]):
+            next_sequence = []
+            for idx, value in enumerate(sequences[-1][1:]):
+                next_sequence.append(value - sequences[-1][idx])
+            sequences.append(next_sequence)
+
+        len_sequences = len(sequences)
+        for idx, sequence in enumerate(sequences[-2::-1]):
+            sequences[len_sequences - idx - 2].append(
+                sequence[-1] + sequences[len_sequences - idx - 1][-1]
+            )
+
+        out += sequences[0][-1]
+
+    return out
+
+
+def part2(data):
+    out = 0
+    histories = [history.split() for history in data]
+    for history in histories:
+        history = [int(v) for v in history]
+        sequences = [history]
+        while not all(v == 0 for v in sequences[-1]):
+            next_sequence = []
+            for idx, value in enumerate(sequences[-1][1:]):
+                next_sequence.append(value - sequences[-1][idx])
+            sequences.append(next_sequence)
+
+        len_sequences = len(sequences)
+        for idx, sequence in enumerate(sequences[-2::-1]):
+            sequences[len_sequences - idx - 2].insert(
+                0, sequence[0] - sequences[len_sequences - idx - 1][0]
+            )
+
+        out += sequences[0][0]
+
+    return out
+
+
+measure_performance("part 1", part1, data)
+measure_performance("part 2", part2, data)
