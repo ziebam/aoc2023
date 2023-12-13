b6ad3217ef2d7fe6ace21b448a7b680f861be0aa
ziebam <ziebamichal@tutanota.com>
2023-12-13 20:22:08 +0100

    Add beautiful solution for day 12
    Hugely inspired by this post on reddit:
    https://www.reddit.com/r/adventofcode/comments/18hbbxe/2023_day_12python_stepbystep_tutorial_with_bonus/

--- day12/day12.py
+++ day12/day12.py
@@ -1,49 +1,87 @@
-import itertools
+# Hugely inspired by the amazing writeup at:
+# https://www.reddit.com/r/adventofcode/comments/18hbbxe/2023_day_12python_stepbystep_tutorial_with_bonus/
+
+import functools
+
+from performance_utils.performance_utils import measure_performance
 
 with open("day12/in12.txt") as in12:
     data = [line.strip() for line in in12.readlines()]
 
 
-def get_combinations(length):
-    yield from itertools.product(*([".#"] * length))
+@functools.cache
+def calculate_possibilities(record, groups):
+    if not groups:
+        if "#" not in record:
+            return 1
+        else:
+            return 0
+
+    if not record:
+        return 0
+
+    next_character = record[0]
+    next_group = groups[0]
+
+    def pound():
+        this_group = record[:next_group]
+        this_group = this_group.replace("?", "#")
+
+        if this_group != next_group * "#":
+            return 0
+
+        if len(record) == next_group:
+            if len(groups) == 1:
+                return 1
+            else:
+                return 0
+
+        if record[next_group] in "?.":
+            return calculate_possibilities(record[next_group + 1 :], groups[1:])
+
+        return 0
+
+    def dot():
+        return calculate_possibilities(record[1:], groups)
+
+    if next_character == "#":
+        possibilities = pound()
+
+    elif next_character == ".":
+        possibilities = dot()
+
+    elif next_character == "?":
+        possibilities = dot() + pound()
+
+    else:
+        raise RuntimeError("Unexpected character!")
+
+    return possibilities
 
 
 def part1(data):
     out = 0
-    for idx, line in enumerate(data):
-        springs, records = line.split()
-        records = [int(r) for r in records.split(",")]
+    for line in data:
+        record, raw_groups = line.split()
+        groups = [int(i) for i in raw_groups.split(",")]
 
-        unknowns = 0
-        for c in springs:
-            if c == "?":
-                unknowns += 1
-
-        combinations = get_combinations(unknowns)
-        for combination in combinations:
-            new_springs = springs[:]
-            for idx in range(len(combination)):
-                new_springs = new_springs.replace("?", combination[idx], 1)
-
-            operational_counts = []
-            current_count = 0
-            for spring in new_springs:
-                if spring == ".":
-                    if current_count > 0:
-                        operational_counts.append(current_count)
-                    current_count = 0
-                else:
-                    current_count += 1
-
-            if current_count > 0:
-                operational_counts.append(current_count)
-
-            if operational_counts == records:
-                out += 1
+        out += calculate_possibilities(record, tuple(groups))
 
     return out
 
 
-print(
-    f"Part 1 answer: \033[92m{part1(data)}\x1b[0m. Too slow for performance measurement.\n"
-)
+def part2(data):
+    out = 0
+    for line in data:
+        raw_record, raw_groups = line.split()
+        record = "?".join(raw_record for _ in range(5))
+        raw_groups = ",".join(raw_groups for _ in range(5))
+        groups = [int(group) for group in raw_groups.split(",")]
+
+        out += calculate_possibilities(record, tuple(groups))
+
+    return out
+
+
+measure_performance("part 1", part1, data)
+measure_performance("part 2", part2, data)
